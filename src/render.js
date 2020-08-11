8740a165ad7d6620efee4ff3293e839187150285
aakif-h <aakifhussaini@gmail.com>
2020-08-10 20:48:18 -0700

    able to view video stream from sources

---
+++ src/render.js
@@ -0,0 +1,45 @@
+// buttons
+
+const videoElement = document.querySelector('video');
+const startBtn = document.getElementById('startBtn');
+const stopBtn = document.getElementById('stopBtn');
+const videoSelectBtn = document.getElementById('videoSelectBtn')
+videoSelectBtn.onclick = getVideoSources;
+
+
+const { desktopCapturer, remote } = require('electron');
+const { Menu } = remote;
+// get the available video sources
+async function getVideoSources() {
+    const inputSources = await desktopCapturer.getSources({
+        types: ['window', 'screen']
+    });
+
+    const videoOptionsMenu = Menu.buildFromTemplate(
+        inputSources.map(source => {
+            return {
+                label: source.name,
+                click: () => selectSource(source)
+            };
+        })
+    );
+    videoOptionsMenu.popup();
+}
+
+async function selectSource(source) {
+    videoSelectBtn.innerText = source.name;
+    const constraints = {
+        audio: false,
+        video: {
+            mandatory: {
+                chromeMediaSource: 'desktop',
+                chromeMediaSourceId: source.id
+            }
+        }
+    };
+
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+
+    videoElement.srcObject = stream;
+    videoElement.play();
+}
